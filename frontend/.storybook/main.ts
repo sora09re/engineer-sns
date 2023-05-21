3cb20226a5a6bb07e2488d71978d2b3c0a54297f
bluesolar1999 <blue.solar1999@gmail.com>
2023-05-21 15:53:58 +0900

    🔧 設定関連ファイルの更新

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -1,5 +1,15 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
+
 const config: StorybookConfig = {
+  webpackFinal: async (config, { configType }) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "../src"),
+    };
+    return config;
+  },
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
