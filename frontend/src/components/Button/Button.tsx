5b11420db6a2b9abc01be5164282ecebeef158c9
bluesolar1999 <blue.solar1999@gmail.com>
2023-05-28 17:09:14 +0900

    🔥 Buttonコンポーネントの削除

--- frontend/src/components/Button/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { Button as MantineButton, MantineProvider } from "@mantine/core";
-
-export const Button = () => {
-  return (
-    <MantineProvider>
-      <MantineButton>投稿する</MantineButton>
-    </MantineProvider>
-  );
-};
