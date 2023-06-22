91ccce73c7087a5382326a4be78a7906b39e0f1b
bluesolar1999 <blue.solar1999@gmail.com>
2023-06-22 19:48:09 +0900

    🏷️ 型Userの定義を修正

--- frontend/src/types/user.ts
+++ frontend/src/types/user.ts
@@ -1,12 +1,15 @@
 export type User = {
   id: number;
-  bio: string;
+  bio?: string;
   createdAt: Date;
   email: string;
   followerCount: number;
   followingCount: number;
+  location?: string;
+  name: string;
   passwordHash: string;
-  profileImageUrl: string;
+  profileImageUrl?: string;
   updatedAt: Date;
   username: string;
+  website?: string;
 };
