ac6774f79e70b2d6a125e4884e57d2e8643d2f0d
bluesolar1999 <blue.solar1999@gmail.com>
2023-07-23 20:48:41 +0900

    🗃️ ログイン情報をSupabaseに持たせるよう実装

--- frontend/src/pages/api/auth/[...nextauth].js
+++ frontend/src/pages/api/auth/[...nextauth].js
@@ -1,8 +1,13 @@
+import { SupabaseAdapter } from "@auth/supabase-adapter";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
 const options = {
-  // Configure one or more authentication providers
+  adapter: SupabaseAdapter({
+    schema: "next_auth",
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY,
+    url: process.env.NEXT_PUBLIC_SUPABASE_URL,
+  }),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
