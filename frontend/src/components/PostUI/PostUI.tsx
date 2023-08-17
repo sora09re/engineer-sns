1912594760106c72e6cd30235951585d33acf433
bluesolar1999 <blue.solar1999@gmail.com>
2023-08-17 16:02:24 +0900

    💄 投稿をクリックすると詳細ページに飛ぶように修正

--- frontend/src/components/PostUI/PostUI.tsx
+++ frontend/src/components/PostUI/PostUI.tsx
@@ -23,52 +23,57 @@ export const PostUI = ({
   const parsedContent = parseContent(post.content);
 
   return (
-    <Paper key={post.id} p="md" shadow="xs" w="full">
-      <Flex>
-        <Avatar
-          src={post.users.profile_image_url}
-          alt="投稿したユーザーのプロフィール画像"
-        />
-        <Space w="md" />
-        <div>
-          <Group spacing="xs">
-            <Text fw={700}>{post.users.name}</Text>
-            <Text color="dimmed">@{post.users.username}</Text>
-            <Text color="dimmed">
-              <DateFormat props={post.created_at} />
-            </Text>
-          </Group>
-          {parsedContent.map((part, index) => {
-            return <ContentPart key={index} part={part} />;
-          })}
-          <Space h="md" />
-          <Group spacing="xl">
-            <Flex align="center">
-              <Link href={`/posts/${post.id}`} style={{ height: "1.2rem" }}>
-                <IconMessageCircle2
+    <Link
+      href={`/posts/${post.id}`}
+      style={{ color: "inherit", textDecoration: "none" }}
+    >
+      <Paper key={post.id} p="md" shadow="xs" w="full">
+        <Flex>
+          <Avatar
+            src={post.users.profile_image_url}
+            alt="投稿したユーザーのプロフィール画像"
+          />
+          <Space w="md" />
+          <div>
+            <Group spacing="xs">
+              <Text fw={700}>{post.users.name}</Text>
+              <Text color="dimmed">@{post.users.username}</Text>
+              <Text color="dimmed">
+                <DateFormat props={post.created_at} />
+              </Text>
+            </Group>
+            {parsedContent.map((part, index) => {
+              return <ContentPart key={index} part={part} />;
+            })}
+            <Space h="md" />
+            <Group spacing="xl">
+              <Flex align="center">
+                <Link href={`/posts/${post.id}`} style={{ height: "1.2rem" }}>
+                  <IconMessageCircle2
+                    size="1.2rem"
+                    cursor="pointer"
+                    color="black"
+                  />
+                </Link>
+                <Space w="xs" />
+                <Text>{post.comments ? post.comments.length : 0}</Text>
+              </Flex>
+              <Flex align="center">
+                <IconThumbUp
                   size="1.2rem"
+                  color={isLikedByCurrentUser ? "#228be6" : "black"}
                   cursor="pointer"
-                  color="black"
+                  onClick={() => {
+                    return handleLikeClick(post.id);
+                  }}
                 />
-              </Link>
-              <Space w="xs" />
-              <Text>{post.comments ? post.comments.length : 0}</Text>
-            </Flex>
-            <Flex align="center">
-              <IconThumbUp
-                size="1.2rem"
-                color={isLikedByCurrentUser ? "#228be6" : "black"}
-                cursor="pointer"
-                onClick={() => {
-                  return handleLikeClick(post.id);
-                }}
-              />
-              <Space w="xs" />
-              <Text>{post.likes.length}</Text>
-            </Flex>
-          </Group>
-        </div>
-      </Flex>
-    </Paper>
+                <Space w="xs" />
+                <Text>{post.likes.length}</Text>
+              </Flex>
+            </Group>
+          </div>
+        </Flex>
+      </Paper>
+    </Link>
   );
 };
