7092d651d8e301e8a8953b0c485275204256f756
bluesolar1999 <blue.solar1999@gmail.com>
2023-08-18 13:36:52 +0900

    💄 コピーボタンの導入とその他UIの変更

--- frontend/src/components/ContentPart/ContentPart.tsx
+++ frontend/src/components/ContentPart/ContentPart.tsx
@@ -1,8 +1,19 @@
-import { Text } from "@mantine/core";
+import { Box, Text } from "@mantine/core";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { monokaiSublime } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
+import { CopyButton } from "@/components/CopyButton/CopyButton";
+
 type ParsedContent = { content: string; type: "text" | "code" };
+
+type ContentPartProps = {
+  part: ParsedContent;
+};
+
+const COMPONENT_MAP = {
+  code: SyntaxHighlighter,
+  text: Text,
+};
 
 export const parseContent = (content: string): ParsedContent[] => {
   const parts = content.split("```");
@@ -15,25 +26,28 @@ export const parseContent = (content: string): ParsedContent[] => {
   });
 };
 
-type ContentPartProps = {
-  part: ParsedContent;
-};
-
-const COMPONENT_MAP = {
-  code: SyntaxHighlighter,
-  text: Text,
-};
-
 export const ContentPart: React.FC<ContentPartProps> = ({ part }) => {
   const Component = COMPONENT_MAP[part.type];
+  const value = part.content.trim();
 
   if (part.type === "code") {
     return (
-      <Component language="htmlbars" style={monokaiSublime}>
-        {part.content}
-      </Component>
+      <Box sx={{ cursor: "auto", position: "relative" }}>
+        <Box sx={{ position: "absolute", right: 10, top: 10 }}>
+          <CopyButton value={value} />
+        </Box>
+        <Component
+          language="htmlbars"
+          style={monokaiSublime}
+          onClick={(event) => {
+            event.stopPropagation();
+          }}
+        >
+          {value}
+        </Component>
+      </Box>
     );
   }
 
-  return <Component>{part.content}</Component>;
+  return <Component>{value}</Component>;
 };
---
+++ frontend/src/components/CopyButton/CopyButton.tsx
@@ -0,0 +1,34 @@
+import {
+  ActionIcon,
+  CopyButton as MantineCopyButton,
+  Tooltip,
+} from "@mantine/core";
+import { IconCheck, IconCopy } from "@tabler/icons";
+
+interface CopyButtonProps {
+  value: string;
+}
+
+export const CopyButton = ({ value }: CopyButtonProps) => {
+  return (
+    <MantineCopyButton value={value} timeout={2000}>
+      {({ copied, copy }) => {
+        return (
+          <Tooltip
+            label={copied ? "Copied" : "Copy"}
+            withArrow
+            position="right"
+          >
+            <ActionIcon color={copied ? "teal" : "gray"} onClick={copy}>
+              {copied ? (
+                <IconCheck size="1.5rem" />
+              ) : (
+                <IconCopy size="1.5rem" />
+              )}
+            </ActionIcon>
+          </Tooltip>
+        );
+      }}
+    </MantineCopyButton>
+  );
+};
