5880d591ddc31bb45171f9c9d719db3dbeeb66a3
bluesolar1999 <blue.solar1999@gmail.com>
2023-08-18 09:24:36 +0900

    💄 投稿の特定の部分をクリックするとページ遷移するように実装

--- frontend/src/components/PostUI/PostUI.tsx
+++ frontend/src/components/PostUI/PostUI.tsx
@@ -1,6 +1,17 @@
-import { Avatar, Flex, Group, Paper, Space, Text } from "@mantine/core";
+import {
+  Avatar,
+  Box,
+  Group,
+  Paper,
+  Space,
+  Text,
+  Tooltip,
+  UnstyledButton,
+} from "@mantine/core";
+import { useHover } from "@mantine/hooks";
 import { IconMessageCircle2, IconThumbUp } from "@tabler/icons";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import {
   ContentPart,
@@ -21,59 +32,92 @@ export const PostUI = ({
   post,
 }: PostUIProps) => {
   const parsedContent = parseContent(post.content);
+  const { hovered: hoveredComments, ref: refComments } = useHover();
+  const { hovered: hoveredLikes, ref: refLikes } = useHover();
+  const router = useRouter();
+
+  const commentsColor = hoveredComments ? "#228be6" : "black";
+  const likesColor = hoveredLikes
+    ? "#37B24D"
+    : isLikedByCurrentUser
+    ? "#37B24D"
+    : "black";
 
   return (
-    <Link
-      href={`/posts/${post.id}`}
-      style={{ color: "inherit", textDecoration: "none" }}
+    <Paper
+      key={post.id}
+      p="md"
+      shadow="xs"
+      w="full"
+      onClick={() => {
+        return router.push(`/posts/${post.id}`);
+      }}
+      style={{ cursor: "pointer" }}
     >
-      <Paper key={post.id} p="md" shadow="xs" w="full">
-        <Flex>
-          <Avatar
-            src={post.users.profile_image_url}
-            alt="投稿したユーザーのプロフィール画像"
-          />
-          <Space w="md" />
-          <div>
-            <Group spacing="xs">
-              <Text fw={700}>{post.users.name}</Text>
-              <Text color="dimmed">@{post.users.username}</Text>
-              <Text color="dimmed">
-                <DateFormat props={post.created_at} />
+      <Group align="start">
+        <Box>
+          <Link href={`/profile/${post.user_id}`}>
+            <Avatar
+              src={post.users.profile_image_url}
+              alt="投稿したユーザーのプロフィール画像"
+            />
+          </Link>
+        </Box>
+        <Box>
+          <Group spacing="xs">
+            <Link
+              href={`/profile/${post.user_id}`}
+              style={{ textDecoration: "none" }}
+            >
+              <Text fw={700} color="black">
+                {post.users.name}
               </Text>
-            </Group>
-            {parsedContent.map((part, index) => {
-              return <ContentPart key={index} part={part} />;
-            })}
-            <Space h="md" />
-            <Group spacing="xl">
-              <Flex align="center">
-                <Link href={`/posts/${post.id}`} style={{ height: "1.2rem" }}>
-                  <IconMessageCircle2
-                    size="1.2rem"
-                    cursor="pointer"
-                    color="black"
-                  />
-                </Link>
-                <Space w="xs" />
-                <Text>{post.comments ? post.comments.length : 0}</Text>
-              </Flex>
-              <Flex align="center">
-                <IconThumbUp
-                  size="1.2rem"
-                  color={isLikedByCurrentUser ? "#228be6" : "black"}
-                  cursor="pointer"
-                  onClick={() => {
-                    return handleLikeClick(post.id);
-                  }}
-                />
-                <Space w="xs" />
-                <Text>{post.likes.length}</Text>
-              </Flex>
-            </Group>
-          </div>
-        </Flex>
-      </Paper>
-    </Link>
+            </Link>
+            <Text color="dimmed">@{post.users.username}</Text>
+            <Text color="dimmed">
+              <DateFormat props={post.created_at} />
+            </Text>
+          </Group>
+          {parsedContent.map((part, index) => {
+            return <ContentPart key={index} part={part} />;
+          })}
+          <Space h="md" />
+          <Group align="center" spacing={40}>
+            <UnstyledButton>
+              <Link
+                href={`/posts/${post.id}`}
+                style={{ textDecoration: "none" }}
+              >
+                <Tooltip label="コメント" position="bottom" withArrow>
+                  <Group ref={refComments} align="center" spacing="sm">
+                    <IconMessageCircle2 size="1.2rem" color={commentsColor} />
+                    <Text color={commentsColor}>
+                      {post.comments ? post.comments.length : 0}
+                    </Text>
+                  </Group>
+                </Tooltip>
+              </Link>
+            </UnstyledButton>
+            <UnstyledButton
+              onClick={(event) => {
+                event.stopPropagation();
+                return handleLikeClick(post.id);
+              }}
+            >
+              <Tooltip
+                label={!isLikedByCurrentUser ? "いいね" : "いいねを取り消す"}
+                position="bottom"
+                withArrow
+              >
+                <Group ref={refLikes} align="center" spacing="sm">
+                  <IconThumbUp size="1.2rem" color={likesColor} />
+                  <Text color={likesColor}>{post.likes.length}</Text>
+                </Group>
+              </Tooltip>
+            </UnstyledButton>
+          </Group>
+        </Box>
+      </Group>
+    </Paper>
   );
 };
