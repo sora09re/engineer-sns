c5c2d472bcdd211db865e93165beac4d35f74196
bluesolar1999 <blue.solar1999@gmail.com>
2023-08-20 16:23:15 +0900

    🏷️

--- frontend/src/types/post.ts
+++ frontend/src/types/post.ts
@@ -5,11 +5,11 @@ export type PostType = {
   id: string;
   comments: PostType[];
   content: string;
-  created_at: string;
+  created_at: Date;
   is_deleted: boolean;
   likes: Like[];
-  parent_post_id: string;
-  updated_at: string;
+  parent_post_id?: string | null;
+  updated_at: Date;
   user_id: string;
   users: User;
 };
