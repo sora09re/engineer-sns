f0d2c90ffb77785755c436d09bad0413ce1d4938
bluesolar1999 <blue.solar1999@gmail.com>
2023-08-27 20:44:33 +0900

    ✨ 投稿削除のAPI実装

--- frontend/src/pages/api/posts/[postId]/index.ts
+++ frontend/src/pages/api/posts/[postId]/index.ts
@@ -6,27 +6,50 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "GET") {
+  if (req.method !== "GET" && req.method !== "DELETE") {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
-  if (req.method === "GET") {
-    const { postId } = req.query;
+  const { postId } = req.query;
 
-    if (!postId) {
-      return res.status(400).json({ error: "postId is required" });
+  if (!postId) {
+    return res.status(400).json({ error: "postId is required" });
+  }
+
+  try {
+    if (req.method === "GET") {
+      const { data: post, error } = await supabase
+        .from("posts")
+        .select("*, users (*), comments: posts (*), likes (*)")
+        .eq("id", postId)
+        .single();
+
+      if (error) {
+        throw error;
+      }
+
+      return res.status(200).json(post);
     }
 
-    const { data: post, error } = await supabase
-      .from("posts")
-      .select("*, users (*), comments: posts (*), likes (*)")
-      .eq("id", postId)
-      .single();
+    if (req.method === "DELETE") {
+      const { data: post, error } = await supabase
+        .from("posts")
+        .delete()
+        .eq("id", postId);
 
-    if (error) {
+      if (error) {
+        throw error;
+      }
+
+      return res.status(200).json(post);
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("API Error:", error);
       return res.status(500).json({ error: error.message });
+    } else {
+      console.error("An unknown error occurred:", error);
+      return res.status(500).json({ error: "An unknown error occurred" });
     }
-
-    return res.status(200).json(post);
   }
 }
