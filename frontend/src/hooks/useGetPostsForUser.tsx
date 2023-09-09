60bc7e52f29a0ebbdc942e29170f84bffd55ced3
bluesolar1999 <blue.solar1999@gmail.com>
2023-09-09 14:46:33 +0900

    🐛 ルーティングの変更

--- frontend/src/hooks/useGetPostsForUser.tsx
+++ frontend/src/hooks/useGetPostsForUser.tsx
@@ -1,10 +1,11 @@
 import useSWR from "swr";
 
+import { baseURL } from "@/utils/baseUrl";
 import { fetcher } from "@/utils/fetcher";
 
 export const useGetPostsForUser = (userId: string | undefined) => {
   const { data, error, isLoading, mutate } = useSWR(
-    `/api/posts/${userId}/`,
+    `${baseURL}/api/profile/posts/${userId}`,
     fetcher
   );
   return { data, error, isLoading, mutate };
