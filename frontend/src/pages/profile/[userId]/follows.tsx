bf293bfb448938c1a38a396bc766abf67676c683
bluesolar1999 <blue.solar1999@gmail.com>
2023-09-11 21:11:15 +0900

    ⚡️ SSRをSWRに修正

--- frontend/src/pages/profile/[userId]/follows.tsx
+++ frontend/src/pages/profile/[userId]/follows.tsx
@@ -1,23 +1,29 @@
 import { Box, Flex } from "@mantine/core";
-import axios from "axios";
-import type { GetServerSideProps, NextPage } from "next";
+import type { NextPage } from "next";
 import { useRouter } from "next/router";
-import { getServerSession } from "next-auth";
 
+import { CenteredLoader } from "@/components/CenteredLoader/CenteredLoader";
 import { Follows } from "@/components/Follows/Follows";
 import { Sidebar } from "@/components/Sidebar/Sidebar";
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
-import type { User } from "@/types/user";
-import { baseURL } from "@/utils/baseUrl";
+import { useGetCurrentUser } from "@/hooks/useGetCurrentUser";
 import { sideBarWidthBase } from "@/utils/sideBarWidth";
 
-interface FollowsPageProps {
-  currentUser: User;
-}
-
-const FollowsPage: NextPage<FollowsPageProps> = ({ currentUser }) => {
+const FollowsPage: NextPage = () => {
+  const {
+    data: currentUser,
+    error: getCurrentUserError,
+    isLoading: getCurrentUserIsLoading,
+  } = useGetCurrentUser();
   const router = useRouter();
   const userId = router.query.userId as string;
+
+  if (!currentUser || getCurrentUserIsLoading) {
+    return <CenteredLoader />;
+  }
+
+  if (getCurrentUserError) {
+    <div>エラーが発生しました。更新を行ってください。</div>;
+  }
 
   return (
     <Flex>
@@ -27,32 +33,6 @@ const FollowsPage: NextPage<FollowsPageProps> = ({ currentUser }) => {
       </Box>
     </Flex>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await getServerSession(context.req, context.res, authOptions);
-
-  if (!session || !session.user || !session.user.id) {
-    console.error("Session or user ID is not available");
-    return {
-      props: {
-        posts: [],
-      },
-    };
-  }
-
-  const currentUserRes = await axios.get(`${baseURL}/api/users/current`, {
-    params: {
-      currentUserId: session?.user?.id,
-    },
-  });
-  const currentUser = currentUserRes.data;
-
-  return {
-    props: {
-      currentUser,
-    },
-  };
 };
 
 export default FollowsPage;
