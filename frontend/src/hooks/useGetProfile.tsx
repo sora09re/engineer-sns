fa0d60f606c6c84aabff4ee86fa5859d6f0b6b9c
bluesolar1999 <blue.solar1999@gmail.com>
2023-09-12 20:54:05 +0900

    🐛 useIdがundefinedのときFetchしないよう修正

--- frontend/src/hooks/useGetProfile.tsx
+++ frontend/src/hooks/useGetProfile.tsx
@@ -5,8 +5,9 @@ import { baseURL } from "@/utils/baseUrl";
 import { fetcher } from "@/utils/fetcher";
 
 export const useGetProfile = (userId: string) => {
+  const shouldFetch = userId !== undefined;
   const { data, error, isLoading, mutate } = useSWR<ProfileType>(
-    `${baseURL}/api/profile/${userId}`,
+    shouldFetch ? `${baseURL}/api/profile/${userId}` : null,
     fetcher
   );
   return { data, error, isLoading, mutate };
