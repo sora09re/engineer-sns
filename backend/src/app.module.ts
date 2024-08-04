345dad435b61961a1599fd4c6605bd06c1d8f5c0
sora <blue.solar1999@gmail.com>
2024-08-04 10:02:58 +0900

    🔥 不要なファイルの削除

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { SearchModule } from './search/search.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [PrismaModule, SearchModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
