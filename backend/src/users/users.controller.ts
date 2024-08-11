081d80924d5ca64b112333f26a41a7f5f03cad67
sora <blue.solar1999@gmail.com>
2024-08-11 10:37:54 +0900

    ✨ ユーザー取得機能のDTO実装

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { Users } from '@prisma/client';
+import { GetUserInput } from 'src/users/dto/users.input';
 import { UsersService } from 'src/users/users.service';
 
 @Controller('users')
@@ -7,7 +8,8 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  async getUser(@Query('userId') userId: string): Promise<Users | null> {
+  async getUser(@Query() getUserInput: GetUserInput): Promise<Users | null> {
+    const { userId } = getUserInput;
     return await this.usersService.getUserByUserId(userId);
   }
 }
