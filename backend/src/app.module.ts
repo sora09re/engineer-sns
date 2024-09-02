d7c23f314dbe9e3674a02ca933232194bf70ba90
sora <blue.solar1999@gmail.com>
2024-09-02 21:51:25 +0900

    🐛 コミット漏れ

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from 'src/modules/auth/auth.module';
 import { PostsModule } from 'src/posts/posts.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { PostsModule } from 'src/posts/posts.module';
     UsersModule,
     AuthModule,
     PostsModule,
+    ProfileModule,
   ],
 })
 export class AppModule {}
