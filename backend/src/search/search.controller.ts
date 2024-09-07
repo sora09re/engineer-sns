7b4d3f2d4c499f6d6c2bb9e3fc5198feb3113069
sora <blue.solar1999@gmail.com>
2024-09-08 08:46:51 +0900

    🔥 認証ガードを外す

--- backend/src/search/search.controller.ts
+++ backend/src/search/search.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { Posts, Users } from '@prisma/client';
-import { GithubAuthGuard } from 'src/modules/auth/guards/github.guard';
 import { SearchInput } from 'src/search/dto/search.input';
 import { SearchService } from 'src/search/search.service';
 
@@ -8,14 +7,12 @@ import { SearchService } from 'src/search/search.service';
 export class SearchController {
   constructor(private readonly searchService: SearchService) {}
 
-  @UseGuards(GithubAuthGuard)
   @Get('users')
   async searchUsers(@Query() searchInput: SearchInput): Promise<Users[]> {
     const { keyword } = searchInput;
     return await this.searchService.searchUsers(keyword);
   }
 
-  @UseGuards(GithubAuthGuard)
   @Get('posts')
   async searchPosts(@Query() searchInput: SearchInput): Promise<Posts[]> {
     const { keyword } = searchInput;
