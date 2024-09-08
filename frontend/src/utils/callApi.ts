1ccaa0fa077ab1a9aff93a081ce85ebc91a5981c
sora <blue.solar1999@gmail.com>
2024-09-08 11:36:11 +0900

    ✨ 共通API呼び出しメソッドの追加・修正

--- frontend/src/utils/callApi.ts
+++ frontend/src/utils/callApi.ts
@@ -1,15 +1,39 @@
+import type { AxiosRequestConfig } from "axios";
 import axios from "axios";
 
 import { apiUrl } from "@/utils/baseUrl";
 
+export const callGetApi = async (
+  endpoint: string,
+  token?: string,
+  options?: AxiosRequestConfig
+) => {
+  const response = await axios.get(endpoint, {
+    ...options,
+    baseURL: apiUrl,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      ...options?.headers,
+    },
+  });
+
+  return response.data;
+};
+
 export const callPostApi = async (
   endpoint: string,
   data: any,
-  token?: string
+  token?: string,
+  options?: AxiosRequestConfig
 ) => {
-  await axios.post(`${apiUrl}${endpoint}`, data, {
+  const response = await axios.post(endpoint, data, {
+    ...options,
+    baseURL: apiUrl,
     headers: {
       Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+      ...options?.headers,
     },
   });
+  return response.data;
 };
