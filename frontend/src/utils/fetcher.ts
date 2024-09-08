20c33a50390ef426e1c766e6270bdcb2af91d026
sora <blue.solar1999@gmail.com>
2024-09-09 08:11:36 +0900

    ✨ 認証ガード用のFetcher追加

--- frontend/src/utils/fetcher.ts
+++ frontend/src/utils/fetcher.ts
@@ -1,6 +1,18 @@
 import { callGetApi } from "@/utils/callApi";
 
+type TokenFetcherProps = {
+  token: string;
+  url: string;
+};
+
 export const fetcher = async (url: string) => {
   const result = await callGetApi(url);
+  return result;
+};
+
+export const tokenFetcher = async (props: TokenFetcherProps) => {
+  const { token, url } = props;
+
+  const result = await callGetApi(url, token);
   return result;
 };
