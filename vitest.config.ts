02ecbc634252156f4df2cde5d328e81953b906b1
sora <blue.solar1999@gmail.com>
2025-03-20 11:07:00 +0900

    ⬆️ テスト環境の整備

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,12 @@
 /// <reference types="vitest"/>
 
 import path from "node:path";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	plugins: [react()],
 	test: {
 		globals: true,
 		environment: "jsdom",
