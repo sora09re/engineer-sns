8bb11a2323b049bef0318744f462a6a0f0791480
sora <blue.solar1999@gmail.com>
2025-04-27 17:02:16 +0900

    🎨 プロフィールページをAppRouterへ移行

---
+++ src/app/profile/[userId]/_components/ProfilePageClient.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { EditProfileModal } from "@/components/Modal/EditProfileModal/EditProfileModal";
+import { PostsList } from "@/components/PostsList/PostsList";
+import { Profile } from "@/components/Profile/Profile";
+import { Sidebar } from "@/components/Sidebar/Sidebar";
+import type { PostType } from "@/types/post";
+import type { ProfileType } from "@/types/profile";
+import type { User } from "@/types/user";
+import { sideBarWidthBase } from "@/utils/sideBarWidth";
+import { Box, Flex } from "@mantine/core";
+
+interface ProfilePageClientProps {
+	currentUser: User;
+	user: ProfileType;
+	postsForUser: PostType[];
+}
+
+export default function ProfilePageClient({
+	currentUser,
+	user,
+	postsForUser,
+}: ProfilePageClientProps) {
+	return (
+		<Flex>
+			<Sidebar currentUser={currentUser} />
+			<Box w="100%" ml={sideBarWidthBase}>
+				<Profile user={user} currentUserId={currentUser.id} />
+				<PostsList posts={postsForUser} currentUserId={currentUser.id} />
+			</Box>
+			<EditProfileModal currentUser={currentUser} />
+		</Flex>
+	);
+}
